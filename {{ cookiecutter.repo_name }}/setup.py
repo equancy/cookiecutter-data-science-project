91aded35cfbd20a7b7a581862dbf41ab7f12ecab
herve <herve.mignot@equancy.com>
2019-02-21 21:39:21 +0100

    Correct license variable name in setup

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -6,5 +6,5 @@ setup(
     version='0.1.0',
     description='{{ cookiecutter.description }}',
     author='{{ cookiecutter.author_name }}',
-    license='{% if cookiecutter.open_source_license == 'MIT' %}MIT{% else %}Equancy{% endif %}',
+    license='{% if cookiecutter.license == 'MIT' %}MIT{% else %}Equancy{% endif %}',
 )
